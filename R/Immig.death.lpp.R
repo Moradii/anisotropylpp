393c38ce8445b8ce9c038b32cb88e3b38f404136
Moradii <m2.moradi@yahoo.com>
2020-10-17 16:25:50 +0200

    Immig.death.lpp doc updated

--- R/Immig.death.lpp.R
+++ R/Immig.death.lpp.R
@@ -6,25 +6,27 @@
 #'
 #' @param L a linear network
 #' @param a anisotropy angle
-#' @param t pi/t is the tolerance
-#' @param alpha immigration arte
+#' @param t pi/t will be considered the angle tolerance
+#' @param alpha immigration arte of newly arrived immigrants
 #' @param d death rate
 #' @param prob_accep probability to accept a new arriving point
-#' @param r r
-#' @param N N
-#' @param strauss strauss
-#' @param burnin burnin
+#' @param r interaction distance between a newly arrived point and the already established ones
+#' @param N a prescribed number of points to be placed
+#' @param strauss  logical, if TRUE an instance of a Strauss process is performed with interaction parameters (gamma) prob_accep and radius r. If FALSE the instance is an anisotropic point pattern
+#' @param burnin the burnin period for the immigration-death process
 #' @param nsim number of patterns to be simulated
 #' @param verbose logical
-#' @param Figure logical
-#' @param C C
+#' @param Figure logical, if TRUE a figure with the evolution of the number of points contained in L are shown to observe the burn-in process
+#' @param C a numeral constant that controls the scale of the  y-axis of Figure
+#' 
+#' 
 #' 
 #' @examples 
-#'  X <- Immig.death.lpp(L=branchnet,a=pi/2,t=10,alpha=1.0,d=0.001,prob_accep=0,r=0.15,N=0,strauss=FALSE,burnin=10000,nsim=1,verbose=TRUE,Figure=TRUE,C=1)
-#'  plot(X,pch=20,col="red",cols="blue",main="")
+#'  X <- Immig.death.lpp(L=branchnet,a=pi/2,t=10,alpha=0.80,d=0.002,prob_accep=0,r=200,N=0,strauss=FALSE,burnin=20,nsim=1,verbose=TRUE,Figure=TRUE,C=0.75)
+#'plot(X,pch=20,col="red",cols="blue",main="")
 
 #' @references Moradi, M., Mateu, J,. and Comas, C. (2020) Directional analysis for point patterns on linear networks. Stat.
-#' @return if \code{nsim=1} a single realization, otherwise a list of realizations of lpp objects
+#' @return if \code{nsim=1} a single realization, otherwise a list of realizations of \link[spatstat]{lpp} objects
 #' @author Carles Comas \email{carles.comas@udl.cat}, Mehdi Moradi \email{m2.moradi@yahoo.com}
 
 
@@ -45,9 +47,9 @@ Immig.death.lpp <- function(L,a=pi/2,t=10,alpha=1.0,d=0.001,prob_accep=1,r=0.2,N
   
   win <- L$window
   
-    Ys <- rpoislpp(100/volume(L),L) #define a newly arrived point
-   Xap <- as.numeric(Ys$data$x[1])
-   Yap <- as.numeric(Ys$data$y[1])
+  Ys <- rpoislpp(100/volume(L),L) #define a newly arrived point
+  Xap <- as.numeric(Ys$data$x[1])
+  Yap <- as.numeric(Ys$data$y[1])
   pppi <- lpp(ppp(as.numeric(Xap),as.numeric(Yap),window=win),L)
   
   nr <- npoints(pppi)
@@ -70,8 +72,15 @@ Immig.death.lpp <- function(L,a=pi/2,t=10,alpha=1.0,d=0.001,prob_accep=1,r=0.2,N
     }
     
     if (verbose) {
-      cat(paste(i1), ",") #cat(paste(i1), "\n")
-      flush.console()
+      
+      if(i1<REPS){
+        cat(paste(i1), ",")
+        flush.console()
+      }else{
+        cat(paste(i1), "\n")
+        flush.console()
+      }
+      
     }
     
     
@@ -107,32 +116,32 @@ Immig.death.lpp <- function(L,a=pi/2,t=10,alpha=1.0,d=0.001,prob_accep=1,r=0.2,N
     
     if(Z<=alpha){      
       prob_set <- 1 #INITIALLY THIS PROBABLITY IS EQUAL TO 1
-            Ys <- rpoislpp(100/volume(L),L) #define newly arrived point
-           Xap <- as.numeric(Ys$data$x[1])
-           Yap <- as.numeric(Ys$data$y[1])
+      Ys <- rpoislpp(100/volume(L),L) #define newly arrived point
+      Xap <- as.numeric(Ys$data$x[1])
+      Yap <- as.numeric(Ys$data$y[1])
       
-            ps <- psp(as.numeric(rep(Xap,nr)),as.numeric(rep(Yap,nr)),as.numeric(pppi$data$x),as.numeric(pppi$data$y),
+      ps <- psp(as.numeric(rep(Xap,nr)),as.numeric(rep(Yap,nr)),as.numeric(pppi$data$x),as.numeric(pppi$data$y),
                 window = pppi$domain$window,check=TRUE)
-           ang <- angles.psp(ps)
-         pppi2 <-pppi
+      ang <- angles.psp(ps)
+      pppi2 <-pppi
       
-        #pppi2 <- pppi2[ang <(a+pi/t) & ang > (a-(pi/t))]
+      #pppi2 <- pppi2[ang <(a+pi/t) & ang > (a-(pi/t))]
       
-         pppi2 <- pppi2[ang < (a-(pi/t)) | ang >(a+pi/t)]
+      pppi2 <- pppi2[ang < (a-(pi/t)) | ang >(a+pi/t)]
       
-            Ys <- lpp(ppp(Xap,Yap,window=win),L)
+      Ys <- lpp(ppp(Xap,Yap,window=win),L)
       
-            Xs <- superimpose(pppi2,Ys) #new lpp
-            nr <- npoints(pppi2)
+      Xs <- superimpose(pppi2,Ys) #new lpp
+      nr <- npoints(pppi2)
       
       if(strauss){
         Xs <- superimpose(pppi,Ys)
         nr <- npoints(pppi)
       }
       
-           dist <- array(0,c(nr+1,nr+1))
+      dist <- array(0,c(nr+1,nr+1))
       dim(dist) <- c(nr+1,nr+1)
-           dist <- pairdist(Xs) ##the vector with the pairwise distance between points of Xs1
+      dist <- pairdist(Xs) ##the vector with the pairwise distance between points of Xs1
       if(nr>0){
         for(i in 1:nr){
           DIST_s <- dist[i,nr+1]
@@ -144,11 +153,11 @@ Immig.death.lpp <- function(L,a=pi/2,t=10,alpha=1.0,d=0.001,prob_accep=1,r=0.2,N
       } #if nr>0
       
       #ONCE prob_set IS COMPUTED THEN WE HAVE TO TEST IF THE POINT IS ACCEPTED OR NOT
-       Z <- runif(1,0,1)
+      Z <- runif(1,0,1)
       Xs <- superimpose(pppi,Ys)
       nr <-npoints(pppi)
       if(Z<=prob_set){
-               pppi <- Xs
+        pppi <- Xs
         death[nr+1] <- 0
       }              
     }#z<=alpha
@@ -161,7 +170,7 @@ Immig.death.lpp <- function(L,a=pi/2,t=10,alpha=1.0,d=0.001,prob_accep=1,r=0.2,N
   
   if(nsim==1){
     out <- pppi
-     nt <- nt
+    nt <- nt
   }
   
   attr(out,"nt") <- nt
